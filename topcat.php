be935c7aecc421637f6d7bb45abdafc18ca27122
Gabriel Mansour <gabrielmansour@gmail.com>
2011-05-24 14:41:44 -0400

    Update TopCat plugin to be compatible with WP 3.1.x and to work with Custom Taxonomies (currently only supports one prioritized taxonomy at a time).

--- topcat.php
+++ topcat.php
@@ -47,7 +47,7 @@ TO DO
 
 function topcat_the_main_category($post_id = 0, $before = '', $after = '') {
 /*	Write the name of the post's main category if there is one, otherwise nothing.
-	Fi there is a main category, also write the before and after strings.
+	If there is a main category, also write the before and after strings.
 */
 	$cat = topcat_get_the_main_category($post_id);
 	if ('' != $cat) {
@@ -83,20 +83,19 @@ function topcat_get_the_main_category($post_id = 0) {
 function topcat_get_the_main_category_slug($post_id = 0) {
 /*	Return the slug name of the post's main category.
 */
+  global $TOPCAT_TAXONOMY;
 	$cat_id = topcat_get_the_main_category_id($post_id);
-	$category = &get_category($cat_id);
-	return $category->category_nicename;
+	//if (!$cat_id) return null; // safety
+	$category = &get_term($cat_id, $TOPCAT_TAXONOMY);
+	return $category->slug;
 }
 
+/**	Return ID of the post's main category.
+ */
 function topcat_get_the_main_category_id($post_id = 0) {
-/*	Return ID of the post's main category.
-*/
-	global $post, $wpdb;
-	if (0 != $post_id) {
-		$category_id = $wpdb->get_var("SELECT post_category FROM $wpdb->posts WHERE ID = $post_id");
-	} else {
-		$category_id = $post->post_category;
-	}
+	global $post, $TOPCAT_TAXONOMY;
+	if ($post_id == 0) $post_id = $post->ID; 
+	$category_id = intval( get_post_meta($post_id, "main_$taxonomy", true) );
 	return $category_id;
 }
 
@@ -106,18 +105,37 @@ function topcat_get_the_main_category_id($post_id = 0) {
 	Machinery to set and display the main category of posts in the
 	admin interface.
 */
+add_action('init', 'add_main_category_support');
+function add_main_category_support(){
+  add_main_term_support('category');
+}
 
-// Add the radio buttons to the edit form (using DOM). This goes in the
-// footer but might be better off in the edit form actions.
-add_filter('admin_footer', 'tguy_tc_add_radios');
+function add_main_term_support($taxonomy) {
+  global $TOPCAT_TAXONOMY;
+  
+  if (taxonomy_exists($taxonomy)) {
+    $TOPCAT_TAXONOMY = $taxonomy;
+  } else {
+    return new WP_Error('nonexistent-taxonomy', "A taxonomy named '$taxonomy' doesn't exist.", $taxonomy);
+  }
 
-// Add the old main category to the edit form
-add_action('simple_edit_form',   'tguy_tc_add_hidden_field');
-add_action('edit_form_advanced', 'tguy_tc_add_hidden_field');
+  // Add the radio buttons to the edit form (using DOM). This goes in the
+  // footer but might be better off in the edit form actions.
+  add_filter('admin_footer', 'tguy_tc_add_radios');
 
-// Save the main category when saving a new post or editing an old one
-add_action('save_post', 'tguy_tc_save_main_category');
-add_action('edit_post', 'tguy_tc_save_main_category');
+  // Add the old main category to the edit form
+  add_action('quick_edit_custom_box',   'tguy_tc_add_hidden_field');
+  add_action('bulk_edit_custom_box',   'tguy_tc_add_hidden_field');
+
+  add_action('edit_form_advanced', 'tguy_tc_add_hidden_field');
+  add_action('edit_page_form', 'tguy_tc_add_hidden_field'); // for Pages
+  
+
+  // Save the main category when saving a new post or editing an old one
+  add_action('save_post', 'tguy_tc_save_main_category');
+  add_action('edit_post', 'tguy_tc_save_main_category');
+}
+
 
 function tguy_tc_add_radios() {
 /*	If the current page is post.php, add radio buttons for all
@@ -129,108 +147,72 @@ function tguy_tc_add_radios() {
 	||  strpos($_SERVER['REQUEST_URI'], 'post-new.php')) { // post-new for WP 2.1
 	// For details on the createNamedElement() function, see
 	// http://www.thunderguy.com/semicolon/2005/05/23/setting-the-name-attribute-in-internet-explorer/
+	global $TOPCAT_TAXONOMY, $post_ID;
 ?>
-<script language="JavaScript" type="text/javascript"><!--
-
-function createNamedElement(type, name) {
-   var element = null;
-   // Try the IE way; this fails on standards-compliant browsers
-   try {
-      element = document.createElement('<'+type+' name="'+name+'">');
-   } catch (e) {
-   }
-   if (!element || element.nodeName != type.toUpperCase()) {
-      // Non-IE browser; use canonical method to create named element
-      element = document.createElement(type);
-      element.name = name;
-   }
-   return element;
-}
-
-function addRadio(catBox) {
-// Add a "main category" radio button before the given checkbox
-// (which sits inside a label). Return true on success, else false.
-	var cat = catBox.value;
-	var catLabel = catBox.parentNode;
-	var theButton = createNamedElement('input', 'main_category');
-	if (!theButton) {
-		return false;
-	}
-	theButton.type = 'radio';
-	theButton.value = cat;
-	theButton.id = "main_category-"+cat;
-	//theButton.onclick = if radio is now checked, check the corresponding checkbox
-	//theButton.title = help text? does this work?
-
-	// insert radio button before label
-	catLabel.style.display = 'inline';
-	catLabel.parentNode.insertBefore(theButton, catLabel);
-
-	return true;
-}
+<script type="text/javascript"><!--
+jQuery(function($){
 
 // Get all the checkboxes into a separate array now, since the getElementsByTagName()
 // return value will be mutated when we start adding the radio buttons.
-var catBoxes = new Array();
-var categoryDiv = document.getElementById("categorydiv");
-if (categoryDiv) {
-	var inputs = categoryDiv.getElementsByTagName("input");
-	for (var i = 0; i < inputs.length; ++i) {
-		var input = inputs[i];
-		// Make sure it really is a category checkbox
-		if (input.type == "checkbox"
-		&& (   input.id ==    "category-"+input.value // up to WordPress 2.0.5
-			|| input.id == "in-category-"+input.value)) { // WordPress 2.1+
-			catBoxes[catBoxes.length] = input;
-		}
-	}
-}
+$('input:checkbox', '#<?php echo esc_js($TOPCAT_TAXONOMY) ?>div').each(function(){
+  // Make sure it really is a category checkbox
+  if (this.id == "in-<?php echo esc_js($TOPCAT_TAXONOMY) ?>-"+this.value) {
+    // Add a "main category" radio button before the given checkbox
+    // (which sits inside a label). Return true on success, else false.
+    var catCheckbox = this,
+        cat = catCheckbox.value,
+  	    catLabel = catCheckbox.parentNode,
+  	    $button = $('<input>', {
+      	  'type': 'radio',
+      	  'name': "main_<?php echo esc_js($TOPCAT_TAXONOMY) ?>",
+      	  'val': cat, 
+      	  'id': "main_<?php echo esc_js($TOPCAT_TAXONOMY) ?>-"+cat,
+      	  'click': function(e){
+      	    $(catCheckbox).attr('checked', true);
+      	  },
+      	  'title': 'Set ' + $(catCheckbox).text() + ' as the main <?php echo esc_js($TOPCAT_TAXONOMY) ?>'
+      	}).css('display', 'inline').insertBefore(catLabel);
+  }
+});
 
-// Add a radio button before each checkbox
-for (var i = 0; i < catBoxes.length; ++i) {
-	addRadio(catBoxes[i]);
-}
+// Select the main category's radio button:
+$("#old_main_<?php echo esc_js($TOPCAT_TAXONOMY) ?>").each(function(){
+	$("input[type=radio]#main_<?php echo esc_js($TOPCAT_TAXONOMY) ?>-"+this.value, '#<?php echo esc_js($TOPCAT_TAXONOMY) ?>div')
+	  .attr('checked', true);
+});
 
-// Select the main category's radio button
-var topCatElement = document.getElementById("old_main_category");
-if (topCatElement) {
-	var topCat = topCatElement.value;
-	var topCatRadio = document.getElementById("main_category-"+topCat);
-	if (topCatRadio) {
-		topCatRadio.checked = true;
-	}
-}
 
+});
 //--></script>
 <?php
 	}
 }
 
-/*	Add a hidden field with id="old_main_category" and value read from
+/*	Add a hidden field with id="old_main_$TAXONOMY" and value read from
 	the database to the current page.
 */
 function tguy_tc_add_hidden_field() {
-	global $wpdb, $post_ID;
-	$topCat = $wpdb->get_var("SELECT post_category FROM $wpdb->posts WHERE ID='$post_ID'");
-	echo '<input type="hidden" name="old_main_category" id="old_main_category" value="'.$topCat.'" />';
+	global $post_ID, $TOPCAT_TAXONOMY;
+	$topCat = intval( get_post_meta($post_ID, "main_$TOPCAT_TAXONOMY", true) );
+	echo '<input type="hidden" name="old_main_'.$TOPCAT_TAXONOMY.'" id="old_main_'.$TOPCAT_TAXONOMY.'" value="'.$topCat.'" />';
 }
 
 /*	Save the POSTed value of the main category to the given post ID.
 */
 function tguy_tc_save_main_category($post_ID) {
-	global $wpdb;
-	$main_category = 0;
-	if (isset($_POST['main_category'])) {
-		$main_category = (int) $_POST['main_category'];
+  global $TOPCAT_TAXONOMY;
+	$main_term = 0;
+	if (isset($_POST["main_$TOPCAT_TAXONOMY"])) {
+		$main_term = (int) $_POST["main_$TOPCAT_TAXONOMY"];
 	} else {
 		// If there's only one category, then make it the main one
-		$catArray = get_the_category($post_ID);
-		if (1 == count($catArray)) {
-			$main_category = (int) $catArray[0]->cat_ID;
+		$terms = get_the_terms($post_ID, $TOPCAT_TAXONOMY);
+		if ( !is_wp_error($terms) && (1 == count($terms)) ) {
+			$main_term = (int) $terms[0]->term_id;
 		}
 	}
-	if (0 < $main_category) {
-		$wpdb->query("UPDATE $wpdb->posts SET post_category = '" . $main_category . "' WHERE ID = '$post_ID'");
+	if (0 < $main_term) {
+		update_post_meta($post_ID, "main_$TOPCAT_TAXONOMY", $main_term);
 	}
 }
 
